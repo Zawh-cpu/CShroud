26f714932d882e2c4d904450c199968a7decd160
zawh <zawh.abc@gmail.com>
2025-01-04 01:53:17 +0300

    Added planner, services

--- CShroud/Infrastructure/Interfaces/IRateManager.cs
+++ CShroud/Infrastructure/Interfaces/IRateManager.cs
@@ -1,6 +1,11 @@
+using CShroud.Infrastructure.Data.Entities;
+
 namespace CShroud.Infrastructure.Interfaces;
 
 public interface IRateManager
 {
-    
+    public void UpdateRate(User user, uint newRateId)
+    {
+        
+    }
 }

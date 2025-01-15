e826faaf13e4ccbe0f867f9331e9f739af013c46
zawh <zawh.abc@gmail.com>
2025-01-16 02:22:15 +0300

    w

---
+++ CShroudApp/Presentation/Ui/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CShroudApp.Presentation.Ui.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

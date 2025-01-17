055dde030ec7054e53118498361c00f02f38fe6e
zawh <zawh.abc@gmail.com>
2025-01-17 11:41:41 +0300

    Client modification - Swapping to DI way

--- CShroudApp/Presentation/Ui/loader.cs
+++ CShroudApp/Presentation/Ui/loader.cs
@@ -2,14 +2,14 @@
 using System;
 
 namespace CShroudApp.Presentation.Ui;
-/*
+
 sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
+    public static void Run(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
@@ -18,4 +18,4 @@ sealed class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
-}*/
+}
--- CShroudApp/Program.cs
+++ CShroudApp/Program.cs
@@ -1,20 +1,26 @@
 ﻿using CShroudApp.Core.Domain.Entities;
 using CShroudApp.Infrastructure.Interfaces;
 using CShroudApp.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 var host = Host.CreateApplicationBuilder();
 
 host.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
+
+host.Services.Configure<VpnCoreConfig>(host.Configuration.GetSection("VpnCore"));
+
+
+host.Services.AddSingleton(resolver =>
+    resolver.GetRequiredService<IOptions<VpnCoreConfig>>().Value);
+
 host.Services.AddSingleton<ApplicationConfig>(provider => new ApplicationConfig()
 {
     WorkingFolder = Environment.CurrentDirectory
 });
-
-
-host.Services.AddSingleton<VpnCoreConfig>(provider => new VpnCoreConfig());
 
 host.Services.AddSingleton<IProcessManager, ProcessManager>();
 host.Services.AddSingleton<IVpnCore, VpnCore>();
@@ -27,3 +33,5 @@ var app = host.Build();
 var core = app.Services.GetRequiredService<ICore>();
 core.Initialize();
 core.Start();
+
+CShroudApp.Presentation.Ui.Program.Run(new string[0]);
